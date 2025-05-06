16a3cbf1e843cd094b2824158013f3d381d2bbfa
Jack Danna <jackdanna78@gmail.com>
2025-05-06 18:40:49 -0400

    Fixing

--- src/Client/Character copy.fs
+++ src/Client/Character copy.fs
@@ -5,24 +5,32 @@ open CustomizableAttribute
 // Model
 type Character = {
     CharacterName: string
+    CharacterArtURL: string
     CustomizableAttribute: CustomizableAttribute
 }
 
 let init () = {
     CharacterName = ""
+    CharacterArtURL = ""
     CustomizableAttribute = CustomizableAttribute.init "Strength"
 }
 
 // Update
 type Msg =
     | ModifyCharacterName of string
+    | ModifyCharacterArtURL of string
     | CustomizableAttributeMsg of CustomizableAttribute.Msg
 
 let update (msg: Msg) (model: Character) =
+
     match msg with
     | ModifyCharacterName newCharacterName -> {
         model with
             CharacterName = newCharacterName
+      }
+    | ModifyCharacterArtURL newCharacterArtURL -> {
+        model with
+            CharacterArtURL = newCharacterArtURL
       }
     | CustomizableAttributeMsg msg -> {
         model with
@@ -43,6 +51,18 @@ let view (model: Character) (dispatch: Msg -> unit) =
                 prop.placeholder "Enter Character Name..."
                 prop.value model.CharacterName
                 prop.onTextChange (ModifyCharacterName >> dispatch)
+            ]
+            Html.div [
+                prop.className "flex flex-col items-center"
+                prop.children [
+                    Html.img [ prop.src model.CharacterArtURL; prop.style [ style.width 800 ] ]
+                    Daisy.input [
+                        prop.type'.text
+                        prop.placeholder "Enter Character Art URL..."
+                        prop.value model.CharacterArtURL
+                        prop.onTextChange (ModifyCharacterArtURL >> dispatch)
+                    ]
+                ]
             ]
             CustomizableAttribute.view model.CustomizableAttribute (CustomizableAttributeMsg >> dispatch)
         ]
