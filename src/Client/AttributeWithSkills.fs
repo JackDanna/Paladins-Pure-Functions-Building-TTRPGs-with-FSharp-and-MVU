256b06ef129ba587912c768c17e1ed4cb52b4fd5
Jack Danna <jackdanna78@gmail.com>
2025-05-07 19:13:53 -0400

    Fixed coloring

--- src/Client/AttributeWithSkills.fs
+++ src/Client/AttributeWithSkills.fs
@@ -42,6 +42,7 @@ open Feliz.DaisyUI
 let view (model: AttributeWithSkills) (dispatch: Msg -> unit) =
     Daisy.card [
         card.border
+        color.bgBase200
         prop.className "flex items-center m-2"
         Daisy.table [
             Html.thead [
