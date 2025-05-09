9578979ad8414282e1866e4c0c94c8900578aac3
Jack Danna <jackdanna78@gmail.com>
2025-05-09 09:46:11 -0400

    Fixing

--- src/Client/Skill.fs
+++ src/Client/Skill.fs
@@ -8,17 +8,17 @@ let init name = { SkillName = name; Value = 0 }
 // Update
 type Msg =
     | ModifyName of string
-    | AddOneToValue of ceiling: option<int>
-    | MinusOneToValue of celing: option<int>
+    | AddOneToValue of governingAttributeLevel: option<int>
+    | MinusOneToValue of governingAttributeLevel: option<int>
 
 let update (msg: Msg) (model: Skill) =
 
     match msg with
     | ModifyName newName -> { model with SkillName = newName }
-    | AddOneToValue(Some ceiling) ->
+    | AddOneToValue(Some governingAttributeLevel) ->
         let valuePlusOne = model.Value + 1
 
-        if valuePlusOne > ceiling then
+        if valuePlusOne > governingAttributeLevel then
             model
         else
             { model with Value = valuePlusOne }
@@ -27,8 +27,8 @@ let update (msg: Msg) (model: Skill) =
         let valuePlusOne = model.Value - 1
 
         match optionalCeiling with
-        | Some ceiling ->
-            if valuePlusOne > ceiling then
+        | Some governingAttributeLevel ->
+            if valuePlusOne > governingAttributeLevel then
                 model
             else
                 { model with Value = valuePlusOne }
